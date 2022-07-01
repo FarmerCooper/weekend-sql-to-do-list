46c57441317a977520710b1ea4ad80cd1bf7fe76
Diego Baeza <diegob12@live.com>
2022-06-30 20:33:44 -0500

    add GET request with AJAX

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,33 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('Document ready/jQuery in the house');
+
+    // On page load, get tasks
+    getTasks();
+}
+
+// Get tasks
+function getTasks() {
+    // empty the table
+    $('#taskList').empty();
+
+    $.ajax({
+        method: 'GET',
+        url: '/tasks'
+    }).then(function (response) {
+        console.log('Response from GET /tasks', response);
+
+        //append data to the DOM
+        for (let i=0; i<response.length; i++) {
+            $('#taskList').append(`
+                <tr>
+                    <td>${response[i].task}</td>
+                    <td>${response[i].due}</td>
+                    <td>${response[i].completion}</td>
+                </tr>
+            `);
+        }
+    });
 }
 
 // Create a front end experience that allows a user to create a Task.
--- server/routes/to-do.router.js
+++ server/routes/to-do.router.js
@@ -3,4 +3,17 @@ const router = express.Router();
 
 const pool = require('../modules/pool');
 
+//get songs from server
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "things_to_do" ORDER BY "due";';
+    pool.query(queryText)
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((error) => {
+            console.log('Error in GET query,', error);
+            res.sendStatus(500);
+        })
+})
+
 module.exports = router;
